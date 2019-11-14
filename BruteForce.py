0a51c2ee0e282482cdca11d36699ef8d1d3bc2b1
cesarborroch <57728329+cesarborroch@users.noreply.github.com>
2019-11-14 15:12:26 -0300

    New
    First version

---
+++ BruteForce.py
@@ -0,0 +1,19 @@
+def toPassword(dec):
+    digits = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    x = int((dec % len(digits))) - 1
+    rest = int(dec / len(digits))
+    if (rest == 0):
+        return digits[x]
+    return toPassword(rest) + digits[x]
+
+password = 'pass123'
+
+attempt = 0;
+
+while password != toPassword(attempt):
+    # Imprime 1 tentativa a cada meio milhao
+    if(attempt % 500000 == 0):
+        print(toPassword(attempt))
+    attempt += 1
+
+print('Password found is "{}", after {} attempts.'.format(toPassword(attempt), attempt))
